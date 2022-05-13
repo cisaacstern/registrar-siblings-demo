e5a7f2854bf90bad0c105db75dc31a579a25200f
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-05-13 16:51:22 -0700

    second commit

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
+import os
+
 import click
 import subprocess
 
 @click.command()
 @click.option('--tag', prompt='notebook tag',
               help='The pangeo notebook tag to pull.')
-def pull_img(tag):
+def pull_and_run(tag):
     """Pull the specified pangeo-notebook tag."""
 
     def check_output(command):
         output = subprocess.check_output(command)
         click.echo(output)
 
-    img = f"pangeo/pangeo-notebook:{tag}"
-    click.echo(f"Pulling '{img}'!")
-    check_output(["docker", "pull", img])
+    recipe_handler_image = f"pangeo/pangeo-notebook:{tag}"
+    click.echo(f"Pulling '{recipe_handler_image}'!")
+    check_output(["docker", "pull", recipe_handler_image])
     
-
+    check_output(
+        [
+            "docker",
+            "run",
+            "-it",
+            "-v",
+            "submodule:/home/jovyan/submodule",
+            recipe_handler_image,
+            "python3",
+            "submodule/import_recipe.py",
+        ]
+    )
 
 if __name__ == '__main__':
-    pull_img()
+    pull_and_run()
