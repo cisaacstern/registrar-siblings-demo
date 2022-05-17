92b68cc60ea7f5a2d8193b45dd0e6a11da43616b
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-05-17 11:59:07 -0700

    actually import a recipe in sibling

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM docker:latest
 
 RUN apk update && apk add py3-pip
 
-RUN pip install click
+RUN pip install click pygithub
 
 COPY ./app.py app.py
 
--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import os
 import subprocess
 
 import click
+from github import Github
+
 
 @click.command()
 @click.option('--tag', prompt='notebook tag',
               help='The pangeo notebook tag to pull.')
-def pull_and_run(tag):
+@click.option('--feedstock-spec', prompt='feedstock spec')
+@click.option('--recipe-path', prompt='recipe module path')
+@click.option('--recipe-instance-name', prompt='recipe instance name')
+def pull_and_run(tag, feedstock_spec, recipe_path, recipe_instance_name):
     """Pull the specified pangeo-notebook tag."""
 
     def check_output(command):
@@ -23,6 +28,12 @@ def pull_and_run(tag):
     # Question: how reliable is it that `click.echo` will terminate in "\\n"?
     container_id = container_id.decode(encoding="utf-8").replace("\\n", "")
 
+    # Get the recipe module as plain text
+    recipe_module = (
+        Github().get_repo(feedstock_spec)
+        .get_contents(recipe_path).decoded_content.decode()
+    )
+
     # Execute all commands within the notebook environment
     cmd_base = f"docker exec {container_id} conda run -n notebook"
     
@@ -37,7 +48,7 @@ def pull_and_run(tag):
         # NOTE: This command could be replaced with something more useful, such as importing a
         # a recipe and printing a list of `dict_object` keys to stdout. Or registering a recipe
         # with Prefect using a provided script.
-        + ["import pangeo_forge_recipes; print(f'{pangeo_forge_recipes.__version__}=')"]
+        + [recipe_module + f"print({recipe_instance_name})"]
     )
 
     # Stop the sibling container
